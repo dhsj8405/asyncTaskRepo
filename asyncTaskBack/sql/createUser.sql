33d7be55be4b9d5aeaf5bd8216402bf5dc27a504
dhsj8405 <dhsj8405@naver.com>
2023-03-03 16:07:16 +0900

    d

--- asyncTaskBack/sql/createUser.sql
+++ asyncTaskBack/sql/createUser.sql
@@ -2,4 +2,4 @@ create user 'scott'@'localhost' identified by 'tiger';
 create user 'scott'@'%' identified by 'tiger';
 grant all privileges on *.* to 'scott'@'localhost';
 grant all privileges on *.* to 'scott'@'%';
-flush privileges;
+flush PRIVILEGES;
